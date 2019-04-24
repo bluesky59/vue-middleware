666fb7e83379a821f5ed2855406e7dcba0b445d5
maqin <1101787440@qq.com>
2019-04-24 17:26:19 +0800

    add 常用工具

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input v-model="msg"/>
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      msg: ''
+    }
+  },
+  watch: {
+    msg: (newval, oldval) => {
+      console.log(newval);
+      console.log(oldval);
+      this.msg = 'hello vue';
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
